bebd50169185483093446bc1672868327662ef91
mazaen <46546251+mazaen@users.noreply.github.com>
2020-12-03 15:36:11 +0900

    適当な入力をappからhtmlに移し、表示する
    適当な入力をappからhtmlに移し、表示する機能

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory,render_template
 import os
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, Unicode, UnicodeText, ForeignKey
@@ -12,6 +12,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route("/index") #アプリケーション/indexにアクセスが合った場合
+def index():
+    
+    materials = {}
+    materials['name'] = "jinja"
+    materials['shop_name']=list(["shop1","shop2","shop3"])
+    materials['value']=list([100,200,300])
+    materials['stock_num'] = list([10,20,30])
+    return render_template('outputs.html',message=materials)
 
 @app.route('/favicon.ico')
 def favicon():

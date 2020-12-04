327678c9f41e782a9bd294b9b5fef504c6e15241
mazaen <46546251+mazaen@users.noreply.github.com>
2020-12-04 13:58:43 +0900

    料理名が入ってきたらその料理に関する食材や在庫を表示
    /showにアクセスすると、food_nameで指定した料理の具材と在庫が一覧で表示される。

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from database import db, db_init
 
 import pymysql
 import service
+from model.ingredient import Ingredient
+from model.product import Product
+from model.shop import Shop
+from model.stock import Stock
 
 app = Flask(__name__)
 
@@ -71,5 +75,37 @@ def initdatabase():
     service.initialize()
     return "done"
 
+@app.route("/show") #アプリケーション/showにアクセスがあった場合
+def show():
+    #データベース接続
+    connection=pymysql.connect(
+        host="localhost",
+        db="jse_intern",
+        user='jseintern',
+        password= 'jseintern+',
+
+    )
+    cursor=connection.cursor()
+    food_name="カレー"
+    product_name_list=[]
+    coming_list= service.getIngredientsAndProductsFromFoodName(food_name)
+    for ing in coming_list:
+        for pro in ing[1]:
+            product_name_list.append(pro.name)
+            
+    #stock情報を取得
+    cursor.execute("SELECT * FROM stock")
+    materials = cursor.fetchall()
+
+
+    stock_dict=[]
+    for i in materials:
+        #stock内のidを名前に変換してHTMLに送る
+        #product_name_listに格納済みの選択された料理の商品のみ表示
+        if str(service.getProductNameFromId(i[1])) in product_name_list:
+            stock_dict.append(list([str(Ingredient.getIngredientFromId(Product.getProductNameFromId(i[1]).id).name),str(service.getProductNameFromId(i[1])),str(service.getShopNameFromId(i[2])),str(i[3]),str(i[4])]))
+
+    return render_template('show.html',food=food_name,message=stock_dict)
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
